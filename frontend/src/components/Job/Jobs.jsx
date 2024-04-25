175b93e0386ad48d459c2e63a8c470ab7fc778d3
Rohit Kumar <rohitkumar190204@gmail.com>
2024-04-25 11:36:59 +0530

    Completed Jobs Section and Home Section

--- frontend/src/components/Job/Jobs.jsx
+++ frontend/src/components/Job/Jobs.jsx
@@ -1,11 +1,51 @@
-import React from 'react'
+import React, { useContext, useEffect, useState } from "react";
+import axios from "axios";
+import { Link, useNavigate } from "react-router-dom";
+import {Context } from "../../main"
 
 const Jobs = () => {
-  return (
-    <div>
-      
-    </div>
-  )
-}
+  const [jobs, setJobs] = useState([])
 
-export default Jobs
+  const {isAuthorised} = useContext(Context)
+
+  const navigateTo = useNavigate();
+
+  useEffect(() => {
+    try {
+      axios
+        .get("http://localhost:4000/api/v1/job/getall", {
+          withCredentials: true,
+        })
+        .then((res) => {
+          setJobs(res.data);
+        });
+    } catch (error) {
+      console.log(error);
+    }
+  }, []);
+  if (!isAuthorised) {
+    navigateTo("/");
+  }
+  return (
+    <section className="jobs page">
+      <div className="container">
+        <h1>ALL AVAILABLE JOBS</h1>
+        <div className="banner">
+          {jobs.jobs &&
+            jobs.jobs.map((element) => {
+              return (
+                <div className="card" key={element._id}>
+                  <p>{element.title}</p>
+                  <p>{element.category}</p>
+                  <p>{element.country}</p>
+                  <Link to={`/job/${element._id}`}>Job Details</Link>
+                </div>
+              );
+            })}
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default Jobs;
